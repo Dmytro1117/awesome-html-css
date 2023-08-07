bb1d16465e36104725584fd73821738b5633316c
Dmytro <moiseenkodmitriy@i.ua>
2023-08-07 16:52:33 +0300

    oifjdsiff

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,11 @@
   const mobileMenubtn = document.querySelector('.mobile-menu__btn');
   const mobileMenu = document.querySelector('.header__menu');
 
+  const navMenu = document.querySelector('.header__site-nav');
+
   mobileMenubtn.addEventListener('click', toggleMenu);
+
+  navMenu.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     mobileMenubtn.classList.toggle('is-open');
