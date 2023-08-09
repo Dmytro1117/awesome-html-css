333d7ff6e0d685330252d2d7b250a05ea42ec561
Dmytro1117 <111579384+Dmytro1117@users.noreply.github.com>
2023-08-09 13:22:22 +0000

    Deploying to gh-pages from @ Dmytro1117/awesome-html-css@0c256e6250fdb499be72aff52de96019902a1a72 🚀

---
+++ index.57d17975.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelector(".mobile-menu__btn"),t=document.querySelector(".header__menu"),n=document.querySelector(".header__site-nav");function o(){innerWidth>=767||(e.classList.toggle("is-open"),t.classList.toggle("is-open"),document.body.classList.toggle("modal-open"))}e.addEventListener("click",o),n.addEventListener("click",o)})();
+//# sourceMappingURL=index.57d17975.js.map
---
+++ index.6d2d9e7c.js
@@ -0,0 +1,2 @@
+!function(){let o=document.querySelector(".up-button");window.onscroll=function(){document.body.scrollTop>300||document.documentElement.scrollTop>300?o.classList.add("up-button--visible"):o.classList.remove("up-button--visible")}}();
+//# sourceMappingURL=index.6d2d9e7c.js.map
